9c397089d260c56eed649bfc4d0be67128731bfe
Jeff Cheng <apisorder@gmail.com>
2024-12-29 12:59:29 -0800

    add two numbers

--- topic-applications/add-two-numbers-X05/add-two-numbers.py
+++ topic-applications/add-two-numbers-X05/add-two-numbers.py
@@ -1,13 +1,18 @@
 
 #   Programmer:     Cheng, Jeff
-#   Last Modified:  05-12-2024 11:59PM
+#   Last Modified:  12-29-2024 12:58AM
 #   Problem:        2. Add Two Numbers
 
-#   You are given two non-empty linked lists representing two non-negative integers. 
-#   The digits are stored in reverse order, and each of their nodes contains a single digit.
+#   You are given two non-empty linked lists representing two 
+#   non-negative integers.
+#    
+#   The digits are stored in reverse order, and each of their nodes 
+#   contains a single digit.
+#
 #   Add the two numbers and return the sum as a linked list.
 
-#   You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+#   You may assume the two numbers do not contain any leading zero, 
+#   except the number 0 itself.
 
 #   Example 1:
 #   Input: l1 = [2,4,3], l2 = [5,6,4]
