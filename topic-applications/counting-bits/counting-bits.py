36fb05a88c76ccfb0bd864f79ba9bc24cc215e5a
Jeff Cheng <apisorder@gmail.com>
2025-01-10 16:20:42 -0800

    counting bits

--- topic-applications/counting-bits/counting-bits.py
+++ topic-applications/counting-bits/counting-bits.py
@@ -1,11 +1,13 @@
 
 #   Programmer:     Cheng, Jeff
-#   Last Modified:  07-24-2024 12:24PM
+#   Last Modified:  01-10-2024 04:19PM
 #   Problem:        338. Counting Bits
-
+#   Complexity:     time -> O(n)
+#                   space -> O(1)
+#
 #   Given an integer n, return an array ans of length n + 1 such that for each i 
 #   (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
-
+#
 #   Example 1:
 #   Input: n = 2
 #   Output: [0,1,1]
@@ -13,7 +15,7 @@
 #   0 --> 0
 #   1 --> 1
 #   2 --> 10
-
+#
 #   Example 2:
 #   Input: n = 5
 #   Output: [0,1,1,2,1,2]
